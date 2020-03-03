935cdfa03bc1e70e3c4980a3f1cb2eec533da5ce
Rhamses <rhamses.soares@gmail.com>
2020-03-03 01:09:21 -0300

    single page for each campaign created.

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+	<section>
+		<section class="level is-mobile is-info fixLevel">
+      <div class="level-right">
+        <div class="level-item">
+          <h1 class="is-5 has-text-centered title">
+            <a href="/#/" >TWITTER #HASHFLAGS 🚩</a>
+          </h1>
+        </div>
+      </div>
+      <div class="level-left">
+        <div class="level-item">
+          <div class="field">
+            <div class="control has-icons-left">
+              <input v-model="search" @input="filterHashflags" class="input" type="search" :placeholder="$t('searchPlaceholder')">
+              <span class="icon is-small is-left">
+                <i class="fas fa-search fa-xs"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+	</section>
+</template>
+
+<style scoped>
+  .fixLevel {
+    padding-bottom: .5em;
+    padding-top: .5em;
+    /*position: fixed;*/
+    top: 0;
+    width: 100%;
+    z-index: 10;
+
+    background-color: #3498db;
+  }
+  
+  .fixLevel .title {
+    color: #fff;
+    font-size: 1em;
+  }
+
+  .fixLevel .title a,
+  .fixLevel .title a:hover,
+  .fixLevel .title a:active,
+  .fixLevel .title a:visited {
+    color: inherit
+  }
+</style>
+
+<script>
+	export default {
+		name: "Header"
+	}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import './plugins/axios'
 import LazyLoad from 'vue-lazyload';
 import App from './App.vue'
 import i18n from './i18n'
+import router from './router'
 Vue.use(LazyLoad);
 
 Vue.config.productionTip = false
 
 new Vue({
   i18n,
+  router,
   render: h => h(App)
 }).$mount('#app')
